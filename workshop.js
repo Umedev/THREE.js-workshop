4a043faa7d73ab84ab59bfa6e822c1d93abe75a8
Emil Sjölander <sjolander.emil@gmail.com>
2014-05-02 16:09:53 +0200

    moved audio methods into helper script

--- workshop.js
+++ workshop.js
@@ -1,9 +1,4 @@
-var camera, scene, renderer, analyser, audioData, averages, audio, cubes, segmentLength;
-
-// let this be a power of 2
-var SEGMENT_COUNT = 16;
-
-var TIME_FRAME = 200;
+var camera, scene, renderer, cubes;
 
 window.onload = function() {
 	initAudio();
@@ -11,68 +6,9 @@ window.onload = function() {
 	animate();
 };
 
-function initAudio() {
-	var ctx;
-	try {
-		ctx = new AudioContext();
-	} catch(e) {
-		ctx = new webkitAudioContext();
-	}
-
-	analyser = ctx.createAnalyser();
-	audio = document.getElementsByTagName('audio').item(0);
-	var audioSrc = ctx.createMediaElementSource(audio);
-	// we have to connect the MediaElementSource with the analyser 
-	audioSrc.connect(analyser);
-	analyser.connect(ctx.destination);
-
-	// the minimum size allowed for fftSize is 32
-	if(SEGMENT_COUNT > 32) {
-		analyser.fftSize = SEGMENT_COUNT;
-	} else {
-		analyser.fftSize = 32;
-	}
-
-	segmentLength = analyser.fftSize / (SEGMENT_COUNT * 2);
-	
-	analyser.smoothingTimeConstant = 0.1;
-
-	// the amplitude of the audio will be saved into this variable each frame
-	audioData = new Uint8Array(analyser.frequencyBinCount);
-	averages = [];
-	for (var i = 0; i < TIME_FRAME; i++) {
-		averages[i] = [];
-		for (var j = 0; j < SEGMENT_COUNT; j++) {
-			averages[i][j] = 0;
-		}
-	}
-	 
-}
-
-function updateFrequencyAverages() {
-
-	analyser.getByteFrequencyData(audioData);
-
-	var avg = [];
-
-	for (var i = 0; i < SEGMENT_COUNT; i++) {
-		avg[i] = 0;
-		for(var j = i*segmentLength; j < (i+1)*segmentLength; j++) {
-			avg[i] += audioData[j];
-		}
-		avg[i] = (avg[i] / 2) / 256;
-	};
-
-	// add the current timeframes averages to the first position in the array
-	averages.splice(0, 0, avg);
-
-	// remove the last element in the array
-	averages.pop();
-}
-
 function initRenderer() {
-	var container = document.createElement( 'div' );
-	document.body.appendChild( container );
+	var container = document.createElement('div');
+	document.body.appendChild(container);
 
 	// create camera
 	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
@@ -146,29 +82,4 @@ function render() {
 	}
 
 	renderer.render(scene, camera);
-}
-
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (h && s === undefined && v === undefined) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return {
-        r: r,
-        g: g,
-        b: b
-    };
 }
