e16b8dfeddeceb3b922b3d49db1f9a3f5cc23594
Emil Sjölander <sjolander.emil@gmail.com>
2014-05-02 14:27:08 +0200

    added shadows

--- workshop.js
+++ workshop.js
@@ -52,7 +52,7 @@ function initRenderer() {
 	document.body.appendChild( container );
 
 	// create camera
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.set(16, 4, 10);
 	camera.up = new THREE.Vector3(0,1,0);
 	camera.lookAt(new THREE.Vector3(8,0,0));
@@ -60,26 +60,55 @@ function initRenderer() {
 	// create scene
 	scene = new THREE.Scene();
 
+	//create a floor
+	var planeGeometry = new THREE.PlaneGeometry(100,100)
+	var cubeMaterial = new THREE.MeshPhongMaterial({color : '#f1f1f1'});
+	var plane = new THREE.Mesh(planeGeometry, cubeMaterial);
+	plane.position.set(0,0,0);
+	plane.rotation.x = -Math.PI/2;
+	plane.receiveShadow = true;
+	scene.add(plane)
+
 	// create cube
-	var geometry = new THREE.CubeGeometry(1, 1, 1);
-	var material = new THREE.MeshBasicMaterial({color : '#f00'});
+	var cubeGeometry = new THREE.CubeGeometry(1, 1, 1);
+	var cubeMaterial = new THREE.MeshPhongMaterial({color : '#2980b9'});
 	cubes = [];
 	for (var i = 0; i < 8; i++) {
-		cubes[i] = new THREE.Mesh(geometry, material);
+		cubes[i] = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 		cubes[i].position.set(i*2,0,0);
+		cubes[i].castShadow = true;
+		cubes[i].receiveShadow = true;
+
 		scene.add(cubes[i]);
 	};
 	
+	// add some ambient light
+	scene.add(new THREE.AmbientLight(0x444444));
 
-	// create light
-	var ambientLight = new THREE.AmbientLight(0x202020);
-	scene.add(ambientLight);
+	// create light looking at the middle cube
+	var light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(16, 2, 10);
+	light.target.position = new THREE.Vector3(8,0,0)
+	light.shadowCameraNear = 1;
+	light.shadowCameraFar = 1000;
+	light.shadowCameraVisible = true
+	light.castShadow = true;
+	light.shadowDarkness = 0.5;
+	light.shadowMapWidth = 2048;
+	light.shadowMapHeight = 1024;
+	light.shadowCameraLeft = -10; // or whatever value works for the scale of your scene
+	light.shadowCameraRight = 10;
+	light.shadowCameraTop = 10;
+	light.shadowCameraBottom = -10;
+	scene.add(light);
 
 	// create renderer
 	renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor(0xf0f0f0);
+	renderer.setClearColor(0xffffff);
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMapEnabled = true;
+	renderer.shadowMapSoft = false;
 
 	// add canvas to dom
 	container.appendChild(renderer.domElement);
